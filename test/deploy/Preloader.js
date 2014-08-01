4d7956148625aaeaf326f76405003ff76395e4e1
Victor Potasso <victorpotasso@gmail.com>
2014-08-01 15:39:05 -0300

    Added regex to replace dots to slashs

--- test/deploy/Preloader.js
+++ test/deploy/Preloader.js
@@ -1,5 +1,5 @@
 (function() {
-  var Class6, Preloader;
+  var Class6, Class7, Preloader;
 
   Class6 = (function() {
     function Class6() {
@@ -10,11 +10,21 @@
 
   })();
 
+  Class7 = (function() {
+    function Class7() {
+      console.log('__Class7__');
+    }
+
+    return Class7;
+
+  })();
+
   Preloader = (function() {
     function Preloader() {
-      var c6;
+      var c6, c7;
       console.log('__Preloader__');
       c6 = new Class6();
+      c7 = new Class7();
     }
 
     return Preloader;
--- test/src/classes/Preloader.coffee
+++ test/src/classes/Preloader.coffee
@@ -1,7 +1,9 @@
+#import lib.ext.Class7
 #import lib2.Class6
 
 class Preloader
 	constructor:()->
 		console.log('__Preloader__')
 		c6 = new Class6()
+		c7 = new Class7()
 new Preloader()
